f70f61ebdad889fb2c9011060c6e6862209729b3
Luke <luke@luke.co.pl>
2017-03-18 21:30:51 +0100

    Fix lng and lat fields format

--- localizefriends/api/forms.py
+++ localizefriends/api/forms.py
@@ -6,8 +6,8 @@ class LocalizeFriendsApiForm(forms.Form):
 
 
 class UpdateLocationForm(LocalizeFriendsApiForm):
-    lng = forms.FloatField(min_value=-180, max_value=180)
-    lat = forms.FloatField(min_value=-90, max_value=90)
+    lng = forms.DecimalField(min_value=-180, max_value=180, max_digits=9, decimal_places=6)
+    lat = forms.DecimalField(min_value=-90, max_value=90, max_digits=8, decimal_places=6)
 
 
 class GetFriendsPositionsForm(LocalizeFriendsApiForm):
--- localizefriends/api/models.py
+++ localizefriends/api/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Location(models.Model):
     user_id = models.IntegerField(db_index=True)
     timestamp = models.DateTimeField(auto_now_add=True)
-    longitude = models.DecimalField(max_digits=8, decimal_places=3)
-    latitude = models.DecimalField(max_digits=8, decimal_places=3)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(max_digits=8, decimal_places=6)
