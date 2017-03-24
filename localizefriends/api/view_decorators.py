e26934dc173cf878787a359d8c4e8fdf9795430f
Luke <luke@luke.co.pl>
2017-03-24 22:39:00 +0100

    Refactor views: create validate_with_form decorator

---
+++ localizefriends/api/view_decorators.py
@@ -0,0 +1,22 @@
+from django.http import JsonResponse
+
+def validate_with_form(form_class):
+    def decorator(view):
+        def wrapper(request, *args, **kwargs):
+            if request.method == 'GET':
+                data = request.GET
+            elif request.method == 'POST':
+                data = request.POST
+            else:
+                raise NotImplementedError()
+            form = form_class(data)
+            if form.is_valid():
+                return view(request, form.cleaned_data, *args, *kwargs)
+            else:
+                return JsonResponse({
+                    'success': False,
+                    'message': 'There were validation errors.',
+                    'errors': form.errors
+                }, status=400)
+        return wrapper
+    return decorator
--- localizefriends/api/views.py
+++ localizefriends/api/views.py
@@ -7,6 +7,7 @@ import facebook
 
 from .models import UserLocation, MeetupProposal, Invitee
 from .forms import *
+from .view_decorators import validate_with_form
 
 from pprint import pprint
 from datetime import datetime
@@ -21,144 +22,116 @@ def index(request):
     })
 
 @require_POST
-def update_location(request):
-    form = UpdateLocationForm(request.POST)
-    if form.is_valid():
-        try:
-            graph = facebook.GraphAPI(access_token=form.cleaned_data['fbtoken'], version='2.8')
-            user = graph.get_object('me')
-        except facebook.GraphAPIError as e:
-            return JsonResponse({
-                'success': False,
-                'message': e.message
-            }, status=403)
-
-        location = UserLocation(
-            user_id = user['id'],
-            longitude = form.cleaned_data['lng'],
-            latitude = form.cleaned_data['lat'])
-        location.save()
-
-        return JsonResponse({
-            'success': True
-        })
-    else:
+@validate_with_form(UpdateLocationForm)
+def update_location(request, cleaned_data):
+    try:
+        graph = facebook.GraphAPI(access_token=cleaned_data['fbtoken'], version='2.8')
+        user = graph.get_object('me')
+    except facebook.GraphAPIError as e:
         return JsonResponse({
             'success': False,
-            'message': 'There were validation errors.',
-            'errors': form.errors
-        }, status=400)
+            'message': e.message
+        }, status=403)
+
+    location = UserLocation(
+        user_id = user['id'],
+        longitude = cleaned_data['lng'],
+        latitude = cleaned_data['lat'])
+    location.save()
+
+    return JsonResponse({
+        'success': True
+    })
 
 @require_GET
-def get_friends_locations(request):
-    form = GetFriendsLocationsForm(request.GET)
-    if form.is_valid():
-        try:
-            graph = facebook.GraphAPI(access_token=form.cleaned_data['fbtoken'], version='2.8')
-            friends_using_app = fetch_friends_using_app(graph)
-        except facebook.GraphAPIError as e:
-            return JsonResponse({
-                'success': False,
-                'message': e.message
-            }, status=403)
-
-        for friend in friends_using_app:
-            try:
-                location = UserLocation.objects.filter(user_id=friend['id']).latest('timestamp')
-                friend['location'] = {
-                    'timestamp_ms': int(location.timestamp.timestamp() * 1000),
-                    'longitude': location.longitude,
-                    'latitude':  location.latitude
-                }
-            except ObjectDoesNotExist:
-                friend['location'] = None
-
-        return JsonResponse({
-            'success': True,
-            'data': friends_using_app
-        })
-    else:
+@validate_with_form(GetFriendsLocationsForm)
+def get_friends_locations(request, cleaned_data):
+    try:
+        graph = facebook.GraphAPI(access_token=cleaned_data['fbtoken'], version='2.8')
+        friends_using_app = fetch_friends_using_app(graph)
+    except facebook.GraphAPIError as e:
         return JsonResponse({
             'success': False,
-            'message': 'There were validation errors.',
-            'errors': form.errors
-        }, status=400)
+            'message': e.message
+        }, status=403)
+
+    for friend in friends_using_app:
+        try:
+            location = UserLocation.objects.filter(user_id=friend['id']).latest('timestamp')
+            friend['location'] = {
+                'timestamp_ms': int(location.timestamp.timestamp() * 1000),
+                'longitude': location.longitude,
+                'latitude':  location.latitude
+            }
+        except ObjectDoesNotExist:
+            friend['location'] = None
+
+    return JsonResponse({
+        'success': True,
+        'data': friends_using_app
+    })
 
 @require_POST
-def create_meetup_proposal(request):
-    form = CreateMeetupProposalForm(request.POST)
-    if form.is_valid():
-        try:
-            graph = facebook.GraphAPI(access_token=form.cleaned_data['fbtoken'], version='2.8')
-            user = graph.get_object('me')
-            friends_using_app = fetch_friends_using_app(graph)
-        except facebook.GraphAPIError as e:
-            return JsonResponse({
-                'success': False,
-                'message': e.message
-            }, status=403)
-
-        friends_using_app_ids = list(map(lambda f: f['id'], friends_using_app))
-        invitees_ids = set(form.cleaned_data['invite'].split(','))
-        for invitee_id in invitees_ids:
-            if invitee_id not in friends_using_app_ids:
-                return JsonResponse({
-                    'success': False,
-                    'message': 'User with id {} is not a friend using app.'.format(invitee_id)
-                }, status=403)
-
-        meetup_proposal = MeetupProposal(
-            organizer_id = user['id'],
-            name = form.cleaned_data['name'],
-            start_time = datetime.fromtimestamp(form.cleaned_data['timestamp_ms'] / 1000, pytz.utc),
-            place_name = form.cleaned_data['place_name'],
-            longitude = form.cleaned_data['lng'],
-            latitude = form.cleaned_data['lat'])
-        meetup_proposal.save()
-
-        for invitee_id in invitees_ids:
-            invitee = Invitee(
-                user_id=invitee_id,
-                meetup_proposal=meetup_proposal)
-            invitee.save()
-
-        return JsonResponse({
-            'success': True
-        })
-    else:
+@validate_with_form(CreateMeetupProposalForm)
+def create_meetup_proposal(request, cleaned_data):
+    try:
+        graph = facebook.GraphAPI(access_token=cleaned_data['fbtoken'], version='2.8')
+        user = graph.get_object('me')
+        friends_using_app = fetch_friends_using_app(graph)
+    except facebook.GraphAPIError as e:
         return JsonResponse({
             'success': False,
-            'message': 'There were validation errors.',
-            'errors': form.errors
-        }, status=400)
+            'message': e.message
+        }, status=403)
+
+    friends_using_app_ids = list(map(lambda f: f['id'], friends_using_app))
+    invitees_ids = set(cleaned_data['invite'].split(','))
+    for invitee_id in invitees_ids:
+        if invitee_id not in friends_using_app_ids:
+            return JsonResponse({
+                'success': False,
+                'message': 'User with id {} is not a friend using app.'.format(invitee_id)
+            }, status=403)
+
+    meetup_proposal = MeetupProposal(
+        organizer_id = user['id'],
+        name = cleaned_data['name'],
+        start_time = datetime.fromtimestamp(cleaned_data['timestamp_ms'] / 1000, pytz.utc),
+        place_name = cleaned_data['place_name'],
+        longitude = cleaned_data['lng'],
+        latitude = cleaned_data['lat'])
+    meetup_proposal.save()
+
+    for invitee_id in invitees_ids:
+        invitee = Invitee(
+            user_id=invitee_id,
+            meetup_proposal=meetup_proposal)
+        invitee.save()
+
+    return JsonResponse({
+        'success': True
+    })
 
 @require_GET
-def get_meetup_proposals(request):
-    form = LocalizeFriendsApiForm(request.GET)
-    if form.is_valid():
-        try:
-            graph = facebook.GraphAPI(access_token=form.cleaned_data['fbtoken'], version='2.8')
-            user = graph.get_object('me')
-        except facebook.GraphAPIError as e:
-            return JsonResponse({
-                'success': False,
-                'message': e.message
-            }, status=403)
-
-        meetups = MeetupProposal.objects.filter(
-            Q(organizer_id=user['id']) | Q(invitee__user_id=user['id'])
-        ).order_by('-start_time')
-
-        return JsonResponse({
-            'success': True,
-            'data': [ meetup.to_api_dict() for meetup in meetups ]
-        })
-    else:
+@validate_with_form(LocalizeFriendsApiForm)
+def get_meetup_proposals(request, cleaned_data):
+    try:
+        graph = facebook.GraphAPI(access_token=cleaned_data['fbtoken'], version='2.8')
+        user = graph.get_object('me')
+    except facebook.GraphAPIError as e:
         return JsonResponse({
             'success': False,
-            'message': 'There were validation errors.',
-            'errors': form.errors
-        }, status=400)
+            'message': e.message
+        }, status=403)
+
+    meetups = MeetupProposal.objects.filter(
+        Q(organizer_id=user['id']) | Q(invitee__user_id=user['id'])
+    ).order_by('-start_time')
+
+    return JsonResponse({
+        'success': True,
+        'data': [ meetup.to_api_dict() for meetup in meetups ]
+    })
 
 # --- views end ---
 
